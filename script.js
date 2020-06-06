688f35692ad233967768be8d4e83694411ab4509
Aram Harutyunyan <you@example.com>
2020-06-06 23:25:24 +0400

    I have added the functionality of Delete buttons

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ const todosContainer = document.querySelector('.container');
 const addTodoItemElement = document.querySelector('.add-todo-item');
 //debugger
 function createTodoItemElement(value) {
+  //debugger
   const li = document.createElement('li'); // creating li HTML element
   li.classList = 'item'; // adding the .item class to the li class list
   const deleteButton = createButtonElement(
     'Delete', // creating delete button name text node
-    () => {console.log('deleting')}
+    deleteTodoItem
   ) // creating delete button
   li.appendChild(document.createTextNode(value));
   li.appendChild(deleteButton); // appending delete button to li as child
@@ -20,10 +21,12 @@ function createTodoItemElement(value) {
  * @param {function} onClick - the onClick function of the button
  */
 function createButtonElement(value, onClick) {  
+  //debugger
   const button = document.createElement('button'); // creating button HTML element
   button.appendChild(document.createTextNode(value)); // appending the value to the button as child  
   button.onclick = onClick // appending onClick function to the button
   todos.pop()
+  document.getElementById("input").value="";
   return button; // return the button
 }
 /**
@@ -40,21 +43,17 @@ function createAddTodoButtonElement(
   const addButtonElement = createButtonFc(value, addTodoItem);
   parent.appendChild(addButtonElement)
 }
-function deleteOrAdd(){
-  if(value=="Add Todo") return addTodoItem();
-  return deleteTodoItem();
-};
+
 function addTodoItem(value, data) { 
-  //debugger
+  
   const inputText= document.getElementById("input").value;
   console.log(inputText)
   todos.push(inputText);
   console.log(todos)
-  renderTodoList(todos);
+  return renderTodoList(todos);
 }
-function deleteTodoItem(value, data) { 
-  const inputText= document.getElementById("input").innerText;
-  todos.push(inputText); 
+function deleteTodoItem(event) { 
+  event.target.parentElement.remove();
 }
 function renderTodoList(data) {
   data.forEach(todo => {
